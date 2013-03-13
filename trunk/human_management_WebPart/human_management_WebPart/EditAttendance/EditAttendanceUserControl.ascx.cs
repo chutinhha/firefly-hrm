c6f3ca4b99da0bac2139b53488cfb2d9a8c5a1f6
tungda01659@fpt.edu.vn <tungda01659@fpt.edu.vn@b746364b-1259-5ddf-bb42-8329c3bb0af4>
2013-03-13 13:56:20 +0000

    Add Manage Employee Attendance Function

---
+++ trunk/human_management_WebPart/human_management_WebPart/EditAttendance/EditAttendanceUserControl.ascx.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SP2010VisualWebPart.EditAttendance
+{
+    public partial class EditAttendanceUserControl : UserControl
+    {
+        public Common com = new Common();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Account"].ToString() == "Admin")
+            {
+                if (Session["Name"] != null)
+                {
+                    try
+                    {
+                        if (!IsPostBack)
+                        {
+                            TextBox7.Text = Session["Name"].ToString();
+                            DataTable dt = com.getData("HumanResources.Attendance", " where EmployeeName=N'" + Session["Name"] 
+                                + "' and PunchIn='" + Session["In"] + "'");
+                            if (dt.Rows.Count > 0)
+                            {
+                                DateTime PunchIn = DateTime.Parse(dt.Rows[0][1].ToString().Trim());
+                                DateTime PunchOut = DateTime.Parse(dt.Rows[0][3].ToString().Trim());
+                                TextBox1.Text = PunchIn.Month + "-" + PunchIn.Day + "-" + PunchIn.Year;
+                                TextBox2.Text = PunchIn.Hour+":"+PunchIn.Minute;
+                                TextBox3.Text = dt.Rows[0][2].ToString().Trim();
+                                TextBox4.Text = PunchOut.Hour+":"+PunchOut.Minute;
+                                TextBox6.Text = dt.Rows[0][4].ToString().Trim();
+                            }
+                            Label5.Text = "";
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Label5.Text = ex.Message;
+                    }
+                }
+                else {
+                    Response.Write("<script language='JavaScript'> alert('Access Denied'); </script>");
+                    Response.Redirect(Session["Account"] + ".aspx", true);
+                }
+            }
+            else
+            {
+                Response.Write("<script language='JavaScript'> alert('Access Denied'); </script>");
+                Response.Redirect(Session["Account"] + ".aspx", true);
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            com.closeConnection();
+            Session["EmployeeName"]=Session["Name"];
+            Session.Remove("Name");
+            Session.Remove("In");
+            Session.Remove("Out");
+            Response.Redirect("AttendanceRecord.aspx",true);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text.Trim() == "" || TextBox2.Text.Trim() == ""
+                || TextBox4.Text.Trim() == "")
+            {
+                Label5.Text = "You must enter punch in and punch out time";
+            }
+            else {
+                try
+                {
+                    DateTime punchIn = DateTime.Parse(TextBox1.Text.Trim() + " " + TextBox2.Text.Trim());
+                    DateTime punchOut = DateTime.Parse(TextBox1.Text.Trim() + " " + TextBox4.Text.Trim());
+                    if (DateTime.Compare(punchIn, punchOut) >= 0)
+                    {
+                        Label5.Text = "Punch Out time must be later than Punch In time";
+                    }
+                    else { 
+                        DataTable dt = com.getData("HumanResources.Attendance"," where EmployeeName=N'"
+                            +Session["Name"].ToString()+"' and PunchIn <='"+TextBox1.Text.Trim() + " " 
+                            + TextBox2.Text.Trim()+"' and PunchOut >='"+TextBox1.Text.Trim() + " " 
+                            + TextBox2.Text.Trim()+"' and PunchIn <> '"+Session["In"].ToString()
+                            + "' and PunchOut <> '"+Session["Out"].ToString()+"'");
+                        DataTable dt1 = com.getData("HumanResources.Attendance", " where EmployeeName=N'"
+                            + Session["Name"].ToString() + "' and PunchIn <='" + TextBox1.Text.Trim() + " "
+                            + TextBox4.Text.Trim() + "' and PunchOut >='" + TextBox1.Text.Trim() + " "
+                            + TextBox4.Text.Trim() + "' and PunchIn <> '" + Session["In"].ToString()
+                            + "' and PunchOut <> '" + Session["Out"].ToString() + "'");
+                        DataTable dt2 = com.getData("HumanResources.Attendance", " where EmployeeName=N'"
+                            + Session["Name"].ToString() + "' and PunchIn >='" + TextBox1.Text.Trim() + " "
+                            + TextBox2.Text.Trim() + "' and PunchOut <='" + TextBox1.Text.Trim() + " "
+                            + TextBox4.Text.Trim() + "' and PunchIn <> '" + Session["In"].ToString()
+                            + "' and PunchOut <> '" + Session["Out"].ToString() + "'");
+                        if (dt.Rows.Count != 0)
+                        {
+                            Label5.Text = "You have Punch In in " + dt.Rows[0][1].ToString()
+                                +" and Punch Out in " + dt.Rows[0][3].ToString()
+                                +",you can not Punch In in " + TextBox1.Text.Trim() + " " 
+                                + TextBox2.Text.Trim();
+                        }
+                        else {
+                            if (dt1.Rows.Count != 0)
+                            {
+                                Label5.Text = "You have Punch In in " + dt1.Rows[0][1].ToString()
+                                + " and Punch Out in " + dt1.Rows[0][3].ToString()
+                                + ",you can not Punch Out in " + TextBox1.Text.Trim() + " "
+                                + TextBox4.Text.Trim();
+                            }
+                            else {
+                                if (dt2.Rows.Count != 0)
+                                {
+                                    Label5.Text = "You have Punch In in " + dt2.Rows[0][1].ToString()
+                                        + " and Punch Out in " + dt2.Rows[0][3].ToString()
+                                        + ",you can not Punch In in " + TextBox1.Text.Trim() + " "
+                                        + TextBox2.Text.Trim() + " and Punch Out in " + TextBox1.Text.Trim() + " "
+                                        + TextBox4.Text.Trim();
+                                }
+                                else {
+                                    try
+                                    {
+                                        com.updateTable("HumanResources.Attendance", " PunchIn='" + TextBox1.Text.Trim() + " "
+                                            + TextBox2.Text.Trim() + "',PunchInNote=N'" + TextBox3.Text.Trim() + "',"
+                                            + "PunchOut='" + TextBox1.Text.Trim() + " " + TextBox4.Text.Trim() + "',"
+                                            + "PunchOutNote=N'" + TextBox6.Text.Trim() + "' where EmployeeName=N'"
+                                            +Session["Name"].ToString()+"' and PunchIn='"+Session["In"].ToString()
+                                            +"' and PunchOut='"+Session["Out"]+"'");
+                                        com.closeConnection();
+                                        Session["EmployeeName"] = Session["Name"];
+                                        Session.Remove("Name");
+                                        Session.Remove("In");
+                                        Session.Remove("Out");
+                                        Response.Redirect("AttendanceRecord.aspx", true);
+                                    }
+                                    catch (Exception ex) {
+                                        Label5.Text = ex.Message;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (FormatException) {
+                    Label5.Text = "Invalid Punch In or Punch Out date";
+                }
+            }
+        }
+    }
+}
---
+++ trunk/human_management_WebPart/human_management_WebPart/PunchAttendance/PunchAttendanceUserControl.ascx.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.ObjectModel;
+
+namespace SP2010VisualWebPart.PunchAttendance
+{
+    public partial class PunchAttendanceUserControl : UserControl
+    {
+        public Common com = new Common();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Account"].ToString() == "Admin" && Session["Name"]!=null)
+            {
+                try
+                {
+                    if (!IsPostBack)
+                    {
+                        TextBox1.Text = Session["Name"].ToString();
+                        Label3.Visible = true;
+                        TextBox2.Visible = true;
+                        Button1.Visible = true;
+                        Label1.Text = "Punch In";
+                        Button2.Text = "In";
+                    }
+                }
+                catch (Exception ex) {
+                    Label8.Text = ex.Message;
+                }
+            }
+            else {
+                Response.Write("<script language='JavaScript'> alert('Access Denied'); </script>");
+                Response.Redirect(Session["Account"] + ".aspx", true);
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Calendar1.Visible = true;
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            TextBox2.Text = Calendar1.SelectedDate.Month.ToString() + "-" + Calendar1.SelectedDate.Day + "-" + Calendar1.SelectedDate.Year;
+            Calendar1.Visible = false;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (TextBox2.Text.Trim() == "")
+            {
+                Label8.Text = "You must enter date";
+            }
+            else {
+                if (TextBox3.Text.Trim() == "")
+                {
+                    Label8.Text = "You must enter time (hh:mm)";
+                }
+                else {
+                    try
+                    {
+                        try
+                        {
+                            DateTime dt = DateTime.Parse(TextBox2.Text.Trim() + " " + TextBox3.Text.Trim());
+                            if (Session["In"] == null)
+                            {
+                                DataTable data = com.getData("HumanResources.Attendance", " where EmployeeName=N'"
+                                    + Session["Name"].ToString() + "' and CAST(DAY(PunchIn) as varchar(50))+'-'"
+                                    + "+CAST(MONTH(PunchIn) as varchar(50))+'-'+CAST(YEAR(PunchIn) as varchar(50)) = '"
+                                    + dt.Day + "-" + dt.Month + "-" + dt.Year + "' and PunchOut >'" + TextBox2.Text.Trim() + " "
+                                    + TextBox3.Text.Trim() + "' order by PunchOut desc");
+                                if (data.Rows.Count > 0)
+                                {
+                                    Label8.Text = "Your last punch out is " + data.Rows[0][3].ToString()
+                                        + ", you must enter a time after this time";
+                                }
+                                else
+                                {
+                                    Session["In"] = TextBox2.Text.Trim() + " " + TextBox3.Text.Trim();
+                                    Session["NoteIn"] = TextBox4.Text.Trim();
+                                    Label1.Text = "Punch Out";
+                                    Button2.Text = "Out";
+                                    TextBox3.Text = "";
+                                    TextBox4.Text = "";
+                                    Calendar1.Visible = false;
+                                    Label3.Visible = false;
+                                    TextBox2.Visible = false;
+                                    Button1.Visible = false;
+                                    Label8.Text = "Your last punch in is " + Session["In"].ToString();
+                                }
+                            }
+                            else {
+                                DateTime dt1 = DateTime.Parse(Session["In"].ToString());
+                                if (dt1.Hour > dt.Hour) {
+                                    Label8.Text = "Your last punch in is " + Session["In"].ToString() 
+                                        + ", you must enter a time afer punch in time";
+                                }
+                                else if (dt1.Hour == dt.Hour)
+                                {
+                                    if (dt1.Minute >= dt.Minute)
+                                    {
+                                        Label8.Text = "Your last punch in is " + Session["In"].ToString()
+                                        + ", you must enter a time afer punch in time";
+                                    }
+                                    else {
+                                        com.insertIntoTable("HumanResources.Attendance","N'"+Session["Name"].ToString()
+                                            +"','"+Session["In"].ToString()+"',N'" + Session["NoteIn"].ToString()
+                                            + "','" + TextBox2.Text.Trim() + " " + TextBox3.Text.Trim()+"'"
+                                            +",N'"+TextBox4.Text.Trim()+"'");
+                                        com.closeConnection();
+                                        Session.Remove("In");
+                                        Session.Remove("NoteIn");
+                                        Session["EmployeeName"] = Session["Name"].ToString();
+                                        Session.Remove("Name");
+                                        Response.Redirect("AttendanceRecord.aspx",true);
+                                    }
+                                }
+                                else {
+                                    com.insertIntoTable("HumanResources.Attendance", "N'" + Session["Name"].ToString()
+                                            + "','" + Session["In"].ToString() + "',N'" + Session["NoteIn"].ToString()
+                                            + "','" + TextBox2.Text.Trim() + " " + TextBox3.Text.Trim() + "'"
+                                            + ",N'" + TextBox4.Text.Trim() + "'");
+                                    com.closeConnection();
+                                    Session.Remove("In");
+                                    Session.Remove("NoteIn");
+                                    Session["EmployeeName"] = Session["Name"].ToString();
+                                    Session.Remove("Name");
+                                    Response.Redirect("AttendanceRecord.aspx",false);
+                                }
+                            }
+                        }
+                        catch (FormatException) {
+                            Label8.Text = "Invalid date time. Try again";
+                        }
+                    }
+                    catch (Exception ex) {
+                        Label8.Text = ex.Message;
+                    }
+                }
+            }
+        }
+    }
+}
