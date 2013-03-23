64d2698f5f0819cda2922a48fb376db1c4f2b370
tungda01659@fpt.edu.vn <tungda01659@fpt.edu.vn@b746364b-1259-5ddf-bb42-8329c3bb0af4>
2013-03-23 05:15:18 +0000

    Add AttendanceSummary

---
+++ trunk/human_management_WebPart/human_management_WebPart/Admin/Attendance/AttendanceSummary/AttendanceSummaryUserControl.ascx.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+namespace SP2010VisualWebPart.Admin.Attendance.AttendanceSummary
+{
+    public partial class AttendanceSummaryUserControl : UserControl
+    {
+        private CommonFunction _com = new CommonFunction();
+        private string _condition = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Account"] == null)
+            {
+                Response.Redirect(Message.AccessDeniedPage);
+            }
+            else
+            {
+                if (Session["Account"].ToString() == "Admin")
+                {
+                    try
+                    {
+                        if (!IsPostBack)
+                        {
+                            txtEmployeeName.Text = "All";
+                            ddlJobTitle.AutoPostBack = true;
+                            lblError.Text = "";
+                            _com.setGridViewStyle(grdData);
+                            _com.SetItemList(Message.JobTitleColumn, Message.TableJobTitle, ddlJobTitle, "",true, "All");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        lblError.Text = ex.Message;
+                    }
+                }
+                else
+                {
+                    Response.Redirect(Message.AccessDeniedPage);
+                }
+            }
+        }
+
+        protected void btnView_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtEmployeeName.Text.Trim() == "")
+                {
+                    lblError.Text = Message.EmployeeNameError;
+                }
+                else {
+                    string condition = "";
+                    if (txtEmployeeName.Text.Trim() == "All") { }
+                    else {
+                        condition = " where p."+Message.NameColumn+" like '%"+txtEmployeeName.Text.Trim()+"%'";
+                    }
+                    if (ddlJobTitle.SelectedValue == "All") { }
+                    else {
+                        if (condition == "")
+                        {
+                            condition = " where j." + Message.JobTitleColumn + " = N'" + ddlJobTitle.SelectedValue + "'";
+                        }
+                        else
+                        {
+                            condition = condition + " and j." + Message.JobTitleColumn + " = N'" + ddlJobTitle.SelectedValue + "'";
+                        }
+                    }
+                    try
+                    {
+                        DateTime dt;
+                        DateTime dt1;
+                        if (Request.Form["txtDateFrom"].ToString().Trim() != "")
+                        {
+                            dt = DateTime.Parse(Request.Form["txtDateFrom"].ToString().Trim());
+                            if (condition == "")
+                            {
+                                condition = " where a." + Message.PunchInColumn + " >='" + dt.Month + "-" + dt.Day + "-"
+                                    + dt.Year + "'";
+                            }
+                            else
+                            {
+                                condition = condition + " and a." + Message.PunchInColumn + " >='" + dt.Month + "-" + dt.Day + "-"
+                                    + dt.Year + "'";
+                            }
+                        }
+                        if (Request.Form["txtDateTo"].ToString().Trim() != "")
+                        {
+                            dt1 = DateTime.Parse(Request.Form["txtDateTo"].ToString().Trim());
+                            dt1 = dt1.AddDays(1.0);
+                            if (condition == "")
+                            {
+                                condition = " where a." + Message.PunchOutColumn + "<='" + dt1.Month + "-" + dt1.Day
+                                    + "-" + dt1.Year + "'";
+                            }
+                            else
+                            {
+                                condition = condition + " and a." + Message.PunchOutColumn + "<='" + dt1.Month + "-" + dt1.Day
+                                    + "-" + dt1.Year + "'";
+                            }
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        lblError.Text = Message.InvalidDate;
+                    }
+                    condition = condition + " order by p." + Message.NameColumn;
+                    _com.bindDataAttendanceSummary("p." + Message.NameColumn + ",a." + Message.PunchInColumn
+                        + ",a." + Message.PunchOutColumn, condition, Message.TableAttendance + " a join "
+                        + Message.TablePerson + " p on a." + Message.BusinessEntityIDColumn + " = p."
+                        + Message.BusinessEntityIDColumn + " join " + Message.TableEmployee + " e on a."
+                        + Message.BusinessEntityIDColumn + " = e." + Message.BusinessEntityIDColumn + " join "
+                        + Message.TableJobTitle + " j on j." + Message.JobIDColumn + "=e." + Message.JobIDColumn, grdData);
+                    if (grdData.Rows.Count > 0)
+                    {
+                        lblError.Text = "";
+                        pnlData.Visible = true;
+                        lblDate.Text = "Search Employee: " + txtEmployeeName.Text.Trim();
+                        if (Request.Form["txtDateFrom"].ToString().Trim() != "") {
+                            lblDate.Text = lblDate.Text + "<br> From " + Request.Form["txtDateFrom"].ToString().Trim();
+                        }
+                        if (Request.Form["txtDateTo"].ToString().Trim() != "") {
+                            lblDate.Text = lblDate.Text + "<br> To " + Request.Form["txtDateTo"].ToString().Trim();
+                        }
+                    }
+                    else
+                    {
+                        lblError.Text = Message.NotExistData;
+                        pnlData.Visible = false;
+                    }
+                }
+            }
+            catch (Exception ex) {
+                lblError.Text = ex.Message;
+            }
+        }
+    }
+}
