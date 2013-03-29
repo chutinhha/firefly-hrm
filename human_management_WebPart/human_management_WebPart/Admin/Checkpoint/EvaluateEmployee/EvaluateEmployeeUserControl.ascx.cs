176bb606dcc502f580b5b97acbadb50a0da53860
tungda01659 <tungda01659@fpt.edu.vn>
2013-03-29 17:37:40 +0000

    Fix bug

--- human_management_WebPart/human_management_WebPart/Admin/Checkpoint/EvaluateEmployee/EvaluateEmployeeUserControl.ascx.cs
+++ human_management_WebPart/human_management_WebPart/Admin/Checkpoint/EvaluateEmployee/EvaluateEmployeeUserControl.ascx.cs
@@ -67,7 +67,7 @@ namespace SP2010VisualWebPart.Admin.Checkpoint.EvaluateEmployee
             else {
                 try
                 {
-                    _com.bindData(Message.BusinessEntityIDColumn+","+Message.NameColumn, " where "+Message.NameColumn+" like N'%"
+                    _com.bindData(Message.BusinessEntityIDColumn+","+Message.NameColumn+","+Message.EmailAddressColumn, " where "+Message.NameColumn+" like N'%"
                         + txtEmployeeName.Text.Trim() + "%'", Message.TablePerson, grdData);
                     if (grdData.Rows.Count > 0)
                     {
