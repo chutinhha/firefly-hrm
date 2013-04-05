1cf2a50d70fb06ee92c817db4c680575325643c6
vinhtt0305@gmail.com <vinhtt0305@gmail.com@b746364b-1259-5ddf-bb42-8329c3bb0af4>
2013-04-05 09:10:52 +0000

    update

--- trunk/human_management_WebPart/human_management_WebPart/Admin/AssignDayOff/AssignLeave/AssignLeaveUserControl.ascx.cs
+++ trunk/human_management_WebPart/human_management_WebPart/Admin/AssignDayOff/AssignLeave/AssignLeaveUserControl.ascx.cs
@@ -14,14 +14,14 @@ namespace SP2010VisualWebPart.Admin.AssignDayOff.AssignLeave
         private CommonFunction _com = new CommonFunction();
         protected void Page_Load(object sender, EventArgs e)
         {
-            /*if (Session["Account"] == null)
+            if (Session["Account"] == null)
             {
                 Response.Redirect(Message.AccessDeniedPage);
             }
             else
             {
                 if (Session["Account"].ToString() == "Admin")
-                {*/
+                {
                     if (Session["TaskName"] == null)
                     {
                         Response.Redirect(Message.AccessDeniedPage);
@@ -42,13 +42,13 @@ namespace SP2010VisualWebPart.Admin.AssignDayOff.AssignLeave
                             lblError.Text = ex.Message;
                         }
                     }
-                /*}
+                }
                 else
                 {
                     Session.Remove("TaskName");
                     Response.Redirect(Message.AccessDeniedPage);
                 }
-            }*/
+            }
         }
 
         protected void btnSearch_Click(object sender, EventArgs e)
