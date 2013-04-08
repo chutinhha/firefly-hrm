dac92160d5dc6b69bbe303aac18293c6c8bd0468
vinhtt0305@gmail.com <vinhtt0305@gmail.com@b746364b-1259-5ddf-bb42-8329c3bb0af4>
2013-04-08 09:19:06 +0000

    Add inforEmployee

---
+++ trunk/human_management_WebPart/human_management_WebPart/Admin/Employee/inforEmployee/inforEmployee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+
+namespace SP2010VisualWebPart.Admin.Employee.inforEmployee
+{
+    [ToolboxItemAttribute(false)]
+    public class inforEmployee : WebPart
+    {
+        // Visual Studio might automatically update this path when you change the Visual Web Part project item.
+        private const string _ascxPath = @"~/_CONTROLTEMPLATES/SP2010VisualWebPart.Admin.Employee/inforEmployee/inforEmployeeUserControl.ascx";
+
+        protected override void CreateChildControls()
+        {
+            Control control = Page.LoadControl(_ascxPath);
+            Controls.Add(control);
+        }
+    }
+}
